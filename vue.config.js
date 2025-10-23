e1f09c5e1ba30fdbdad3318f1fb4b7d78e77da8d
Shedrack Ajaegbu <shedrack3@gmail.com>
2025-10-23 19:08:33 +0100

    fix path issues

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/landing-page/" // Must include the trailing slash
-      : "/",
+  publicPath: "/",
   transpileDependencies: true,
 });
